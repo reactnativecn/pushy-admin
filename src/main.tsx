5c88e22a3fdb89a16e81942b694fdc81f5317278
7c00 <i@7c00.cc>
2021-09-29 00:21:13 +0800

    新增激活页

--- src/main.tsx
+++ src/main.tsx
@@ -58,6 +58,9 @@ export default observer(() => {
         <Layout.Content id="main-body" style={style.body}>
           <div style={{ flex: 1 }}>
             <Switch>
+              <Route path="/active">
+                <pages.active />
+              </Route>
               <Route path="/inactivated">
                 <pages.inactivated />
               </Route>
