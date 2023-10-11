f0af2867e7c2e6984bcdf3aa53ba0f52092f8d7f
sunnylqm <sunnylqm@gmail.com>
2023-10-11 23:03:10 +0800

    fix: endpoint

--- src/request.ts
+++ src/request.ts
@@ -1,22 +1,23 @@
-import { message } from "antd";
-import store from "./store";
+import { message } from 'antd';
+import store from './store';
 
 export default async function request(method: string, path: string, params?: any) {
   method = method.toUpperCase();
-  let url = `http://localhost:8910/${path}`;
+  // let url = `http://localhost:8910/${path}`;
+  let url = `https://update.reactnative.cn/api/${path}`;
   const headers: HeadersInit = {};
   const options: RequestInit = { method, headers };
   if (store.token) {
-    headers["x-accesstoken"] = store.token;
+    headers['x-accesstoken'] = store.token;
   }
   if (params) {
-    if (method == "GET") {
+    if (method == 'GET') {
       params = Object.keys(params)
         .filter((key) => params[key] !== undefined)
         .map((key) => `${key}=${encodeURIComponent(params[key])}`);
-      url += "?" + params.join("&");
+      url += `?${params.join('&')}`;
     } else {
-      headers["content-type"] = "application/json";
+      headers['content-type'] = 'application/json';
       options.body = JSON.stringify(params);
     }
   }
