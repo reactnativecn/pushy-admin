61ed661b233ebd67fe68788df173a4d113632e36
Sunny Luo <sunnylqm@gmail.com>
2024-10-28 14:51:23 +0800

    Update success.tsx

--- src/pages/reset-password/components/success.tsx
+++ src/pages/reset-password/components/success.tsx
@@ -6,7 +6,7 @@ export default function Success() {
       status='success'
       title='密码设置成功，请重新登录'
       extra={[
-        <Button key='login' type='primary' href='/#login'>
+        <Button key='login' type='primary' href='/#/login'>
           登录
         </Button>,
       ]}
