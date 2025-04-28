a050cecad814081d9ccadefd930d201764dd3c28
sunnylqm <sunnylqm@gmail.com>
2025-04-28 10:40:40 +0800

    update TEST_QR_CODE_DOC link for accuracy

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,3 +1,3 @@
 export const PRICING_LINK = "https://pushy.reactnative.cn/pricing.html";
 export const TEST_QR_CODE_DOC =
-  "https://pushy.reactnative.cn/docs/bestpractice#%E6%B5%8B%E8%AF%95%E5%8F%91%E5%B8%83%E4%B8%8E%E5%9B%9E%E6%BB%9A";
+  "https://pushy.reactnative.cn/docs/bestpractice#%E6%B5%8B%E8%AF%95%E4%B8%8E%E5%9B%9E%E6%BB%9A";
